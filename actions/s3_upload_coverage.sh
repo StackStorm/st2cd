b21d1a6d6e763e9268b6a7c78fd3f6104dc66408
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-08-26 17:03:08 -0700

    text/css for css file

--- actions/s3_upload_coverage.sh
+++ actions/s3_upload_coverage.sh
@@ -12,4 +12,14 @@ then
 fi
 
 cd ${COVER_DIR}
-s3cmd put --recursive * s3://${BUCKET}/
+s3cmd -M put --recursive * s3://${BUCKET}/
+
+if [ $? == 0]
+then
+    # S3 doesn't detect content type of CSS files correctly. So set
+    # CSS file content type to text/css
+    s3cmd -m text/css put ${COVER_DIR}/style.css s3://${BUCKET}/
+else
+    echo "Failed uploading coverage report to S3."
+    # Don't fail action
+fi
