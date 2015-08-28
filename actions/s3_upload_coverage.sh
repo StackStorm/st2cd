372032d66dbc68e60b508d3ef19795373184a029
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-08-28 15:43:48 -0700

    We already cded to the cover_dir, so just use style.css

--- actions/s3_upload_coverage.sh
+++ actions/s3_upload_coverage.sh
@@ -14,15 +14,17 @@ fi
 
 BUCKET_PATH=${BUCKET}/${BRANCH}
 
-cd ${COVER_DIR}
+pushd ${COVER_DIR}
 s3cmd -M put --recursive * s3://${BUCKET_PATH}/
 
 if [ $? == 0 ]
 then
     # S3 doesn't detect content type of CSS files correctly. So set
     # CSS file content type to text/css
-    s3cmd -m text/css put ${COVER_DIR}/style.css s3://${BUCKET_PATH}/
+    echo "Setting content type of css file to text/css"
+    s3cmd put -m text/css "style.css" "s3://${BUCKET_PATH}/"
 else
     echo "Failed uploading coverage report to S3."
     # Don't fail action
 fi
+popd
