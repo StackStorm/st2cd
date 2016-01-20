7b24ed4bf0e23a8c222023ed2ff46f4c6a2f73bf
Tomaz Muraus <tomaz@tomaz.me>
2016-01-20 21:16:07 +0100

    Update enterprise tests and add a test case which verified that the default RBAC admin role assignments are installed (aka files are present).

---
+++ actions/scripts/verify_default_rbac_admin_role_assignments_are_present.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+RBAC_ASSIGNMENTS_DIR="/opt/stackstorm/rbac/assignments"
+
+ROOT_CLI_ASSIGNMENT_FILE_PATH="${RBAC_ASSIGNMENTS_DIR}/root_cli.yaml"
+STANLEY_ASSIGNMENT_FILE_PATH="${RBAC_ASSIGNMENTS_DIR}/stanley.yaml"
+ADMIN_ASSIGNMENT_FILE_PATH="${RBAC_ASSIGNMENTS_DIR}/admin.yaml"
+HUBOT_ASSIGNMENT_FILE_PATH="${RBAC_ASSIGNMENTS_DIR}/chatops_bot.yaml"
+
+function admin_role_assignment_exists() {
+    username=$1
+    assignment_file_path="${RBAC_ASSIGNMENTS_DIR}/${username}.yaml"
+
+    # Verify assignment file exists
+    if [ ! -f ${assignment_file_path} ]; then
+        echo "RBAC role assignment file '${assignment_file_path}' for user '${username}' doesn't exist"
+        exit 1
+    fi
+
+    # Verify assignment file contains "admin" role assignment
+    cat ${assignment_file_path} | grep '\- "admin"'
+    exit_code=$?
+
+    if [ ${exit_code} -ne 0 ]; then
+        echo "RBAC role assignment file '${assignment_file_path}' for user '${username}' is missing 'admin' role assignment"
+        exit 1
+    fi
+}
+
+admin_role_assignment_exists "root_cli"
+admin_role_assignment_exists "stanley"
+admin_role_assignment_exists "admin"
+admin_role_assignment_exists "chatops_bot"
+
+echo "All role assignmet files exist"
+exit 0
