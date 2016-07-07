5069522cca02320816aa74ad5a56456ee1161629
Winson Chan <m4d.coder@gmail.com>
2016-07-07 22:37:50 +0000

    Add Active Directory for enterprise auth backend

--- actions/st2_upgrade_to_enterprise.sh
+++ actions/st2_upgrade_to_enterprise.sh
@@ -5,6 +5,11 @@ LICENSE_KEY=$1
 DISTRO=$2
 RELEASE=$3
 PKG_ENV=$4
+LDAP_HOST=$5
+LDAP_BIND_DN=$6
+LDAP_BIND_PASSWD=$7
+LDAP_BASE_OU=$8
+LDAP_GROUP_DN=$9
 REPO=enterprise
 
 if [ "${PKG_ENV}" = "staging" ]; then
@@ -15,16 +20,43 @@ if [ "${RELEASE}" = "unstable" ]; then
     REPO="${REPO}-${RELEASE}"
 fi
 
+# Install enterprise bits
 echo "Downloading from repo ${REPO}..."
 
 if [[ ${DISTRO} = \UBUNTU* ]]; then
-    curl -s https://${LICENSE_KEY}:@packagecloud.io/install/repositories/StackStorm/${REPO}/script.deb.sh | sudo bash
-    sudo apt-get install -y st2flow
-    sudo apt-get install -y st2-auth-ldap
+    curl -s https://${LICENSE_KEY}:@packagecloud.io/install/repositories/StackStorm/${REPO}/script.deb.sh | bash
+    apt-get install -y st2flow
+    apt-get install -y st2-auth-ldap
 else
     curl -s https://${LICENSE_KEY}:@packagecloud.io/install/repositories/StackStorm/${REPO}/script.rpm.sh | sudo bash
-    sudo yum install -y st2flow
-    sudo yum install -y st2-auth-ldap
+    yum install -y st2flow
+    yum install -y st2-auth-ldap
 fi
 
-sudo st2ctl restart
+# Replace the auth section
+echo "Updating st2.conf..."
+sed -i -e '/\[system\]/ p' -e '/\[auth\]/,/\[system\]/ d' /etc/st2/st2.conf
+
+cat <<CONF >> /etc/st2/st2.conf
+
+[auth]
+host = 0.0.0.0
+port = 9100
+use_ssl = False
+debug = False
+enable = True
+logging = /etc/st2/logging.auth.conf
+
+mode = standalone
+
+# Note: Settings below are only used in "standalone" mode
+backend = ldap
+backend_kwargs = {"bind_dn": "${LDAP_BIND_DN}", "bind_password": "${LDAP_BIND_PASSWD}", "base_ou": "${LDAP_BASE_OU}", "group_dns": ["${LDAP_GROUP_DN}"], "id_attr": "samAccountName", "host": "${LDAP_HOST}"}
+
+# Base URL to the API endpoint excluding the version (e.g. http://myhost.net:9101/)
+api_url =
+
+CONF
+
+# Restart st2
+st2ctl restart
