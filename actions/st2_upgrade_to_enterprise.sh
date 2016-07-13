82a42f7486903785e9aacaa2d6388a322b366552
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2016-07-13 12:05:14 -0700

    Allow specific enterprise version to be installed

--- actions/st2_upgrade_to_enterprise.sh
+++ actions/st2_upgrade_to_enterprise.sh
@@ -10,6 +10,7 @@ LDAP_BIND_DN=$6
 LDAP_BIND_PASSWD=$7
 LDAP_BASE_OU=$8
 LDAP_GROUP_DN=$9
+VERSION=${10}  # Should be of the form ${major}.${minor}.${patch}
 REPO=enterprise
 
 if [ "${PKG_ENV}" = "staging" ]; then
@@ -20,22 +21,56 @@ if [ "${RELEASE}" = "unstable" ]; then
     REPO="${REPO}-${RELEASE}"
 fi
 
-# Install enterprise bits
-echo "Downloading from repo ${REPO}..."
+get_apt_pkg_latest_revision() {
+    # Returns string of form 1.5.1-5. Note how this is different from rpm :/
+    local PKG_NAME=$1
+    local PKG_VERSION=$2
 
-if [[ ${DISTRO} = \UBUNTU* ]]; then
-    curl -s https://${LICENSE_KEY}:@packagecloud.io/install/repositories/StackStorm/${REPO}/script.deb.sh | bash
-    apt-get install -y st2flow
-    apt-get install -y st2-auth-ldap
-else
-    curl -s https://${LICENSE_KEY}:@packagecloud.io/install/repositories/StackStorm/${REPO}/script.rpm.sh | sudo bash
-    yum install -y st2flow
-    yum install -y st2-auth-ldap
-fi
+    apt-cache show $PKG_NAME | grep Version | awk '{print $2}' | grep $PKG_VERSION | sort --version-sort | tail -n 1
+}
 
-# Replace the auth section
-echo "Updating st2.conf..."
-sed -i -e '/\[system\]/ p' -e '/\[auth\]/,/\[system\]/ d' /etc/st2/st2.conf
+get_rpm_pkg_name_with_latest_revision() {
+    # Returns string of form st2-0:1.5.1-3.x86_64
+    local PKG_NAME=$1
+    local PKG_VERSION=$2
+
+    yum install -y yum-utils # need repoquery
+    repoquery --show-duplicates $PKG_NAME-$PKG_VERSION* | sort --version-sort | tail -n 1
+}
+
+install_enterprise_bits() {
+    # Install enterprise bits from packagecloud
+    echo "Downloading from repo ${REPO}..."
+
+    if [[ ${DISTRO} = \UBUNTU* ]]; then
+        curl -s https://${LICENSE_KEY}:@packagecloud.io/install/repositories/StackStorm/${REPO}/script.deb.sh | bash
+        if [[ -z $VERSION ]]; then
+            apt-get install -y st2flow
+            apt-get install -y st2-auth-ldap
+        else
+            local FLOW_PKG_VERSION=$(get_apt_pkg_latest_revision st2flow $VERSION)
+            local LDAP_PKG_VERSION=$(get_apt_pkg_latest_revision st2-auth-ldap $VERSION)
+            apt-get install -y st2flow=$FLOW_PKG_VERSION
+            apt-get install -y st2-auth-ldap=$LDAP_PKG_VERSION
+        fi
+    else
+        curl -s https://${LICENSE_KEY}:@packagecloud.io/install/repositories/StackStorm/${REPO}/script.rpm.sh | sudo bash
+        if [[ -z $VERSION ]]; then
+            yum install -y st2flow
+            yum install -y st2-auth-ldap
+        else
+            local FLOW_PKG=$(get_rpm_pkg_name_with_latest_revision st2flow $VERSION)
+            local LDAP_PKG=$(get_rpm_pkg_name_with_latest_revision st2-auth-ldap $VERSION)
+            yum install -y $FLOW_PKG
+            yum install -y $LDAP_PKG
+        fi
+    fi
+}
+
+update_st2_conf() {
+    # Replace the auth section
+    echo "Updating st2.conf..."
+    sed -i -e '/\[system\]/ p' -e '/\[auth\]/,/\[system\]/ d' /etc/st2/st2.conf
 
 cat <<CONF >> /etc/st2/st2.conf
 
@@ -57,6 +92,12 @@ backend_kwargs = {"bind_dn": "${LDAP_BIND_DN}", "bind_password": "${LDAP_BIND_PA
 api_url =
 
 CONF
+}
 
-# Restart st2
-st2ctl restart
+restart_st2() {
+    st2ctl restart
+}
+
+install_enterprise_bits
+update_st2_conf
+restart_st2
