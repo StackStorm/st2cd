b1fac1a87038f8511e6f9ceae4d49ff8c40a0c79
Matt Oswalt <oswaltm@brocade.com>
2017-02-23 12:09:23 -0800

    Modified gitref sed statements to properly replace commented out vars
    Signed-off-by: Matt Oswalt <oswaltm@brocade.com>

--- actions/st2_prep_release_for_st2_pkg.sh
+++ actions/st2_prep_release_for_st2_pkg.sh
@@ -101,8 +101,9 @@ fi
 # Update the st2 version at circle.yml
 CIRCLE_YML_FILE="circle.yml"
 echo "Setting version in ${CIRCLE_YML_FILE} to ${BRANCH}..."
-sed -i -e "s/\(ST2_GITREV:[ ]*\).*/\1${BRANCH}/" ${CIRCLE_YML_FILE}
-sed -i -e "s/\(ST2MISTRAL_GITREV:[ ]*\).*/\1${MISTRAL_VERSION}/" ${CIRCLE_YML_FILE}
+# capture group one is to ensure proper indentation. Capture group two is actual var definition
+sed -i -e "s/\(.*\)# \(ST2_GITREV:[ ]*\).*/\1\2${BRANCH}/" ${CIRCLE_YML_FILE}
+sed -i -e "s/\(.*\)# \(ST2MISTRAL_GITREV:[ ]*\).*/\1\2${MISTRAL_VERSION}/" ${CIRCLE_YML_FILE}
 
 MODIFIED=`git status | grep modified || true`
 if [[ ! -z "${MODIFIED}" ]]; then
