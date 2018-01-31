b4b3453a02f27259e8ecc355482d139fc4510a88
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2018-01-31 15:44:44 -0500

    Set st2_version in package.json to release version

--- actions/bwc_prep_release_for_gui.sh
+++ actions/bwc_prep_release_for_gui.sh
@@ -43,21 +43,19 @@ echo "Creating new branch ${BRANCH}..."
 git checkout -b ${BRANCH} origin/master
 
 
-# SET NEW BWC VERSION INFO
-VERSION_FILE="circle.yml"
-VERSION_STR="git clone -b ${BRANCH} https:\/\/github.com"
+# SET NEW ST2 VERSION INFO
+VERSION_FILE="package.json"
+VERSION_STR="\"st2_version\": \"${VERSION}\""
 
-if [ -f ${VERSION_FILE} ]; then
+VERSION_STR_MATCH=`grep "${VERSION_STR}" ${VERSION_FILE} || true`
+if [[ -z "${VERSION_STR_MATCH}" ]]; then
+    echo "Setting version in ${VERSION_FILE} to ${VERSION}..."
+    sed -i -e "s/\(^  \"st2_version\": \"\).*\(\"\)/\1${VERSION}\2/" ${VERSION_FILE}
+
     VERSION_STR_MATCH=`grep "${VERSION_STR}" ${VERSION_FILE} || true`
     if [[ -z "${VERSION_STR_MATCH}" ]]; then
-        echo "Setting version in ${VERSION_FILE} to ${VERSION}..."
-        sed -i -e "s/git clone https:\/\/github.com/${VERSION_STR}/g" ${VERSION_FILE}
-
-        VERSION_STR_MATCH=`grep "${VERSION_STR}" ${VERSION_FILE} || true`
-        if [[ -z "${VERSION_STR_MATCH}" ]]; then
-            >&2 echo "ERROR: Unable to update the bwc version in ${VERSION_FILE}."
-            exit 1
-        fi
+        >&2 echo "ERROR: Unable to update the st2 version in ${VERSION_FILE}."
+        exit 1
     fi
 fi
 
