89e09b214e9def62de8aaec8a51329727c89f382
Warren Van Winckel <warren.van.winckel@gmail.com>
2018-10-24 23:58:47 -0700

    Fix formatting

--- actions/wait_for_docker_image.sh
+++ actions/wait_for_docker_image.sh
@@ -9,9 +9,9 @@ TRIES=$4
 DELAY=$5
 
 if [[ -z "${ORG}" || -z "${IMAGE}" || -z "${VERSION}" ]]; then
-	echo "ERROR: Missing required parameter"
-	echo "Usage: $0 ORG PROJECT VERSION"
-	exit 1
+    echo "ERROR: Missing required parameter"
+    echo "Usage: $0 ORG PROJECT VERSION"
+    exit 1
 fi
 
 function docker_tag_exists() {
@@ -22,16 +22,16 @@ function docker_tag_exists() {
 i=0
 while ! docker_tag_exists "${ORG}/${IMAGE}" $VERSION;
 do
-	if (( ${TRIES} == 0 )); then
-		sleep ${DELAY}
-		continue
-	fi
-	if (( ++i < ${TRIES} )); then
-		sleep ${DELAY}
-	else
-		echo "ERROR: Failed to find image ${ORG}/${IMAGE}:${VERSION} within requested time."
-		exit 1
-	fi
+    if (( ${TRIES} == 0 )); then
+        sleep ${DELAY}
+        continue
+    fi
+    if (( ++i < ${TRIES} )); then
+        sleep ${DELAY}
+    else
+        echo "ERROR: Failed to find image ${ORG}/${IMAGE}:${VERSION} within requested time."
+        exit 1
+    fi
 done
 
 echo "SUCCESS!"
