44eecf984743a50245ce2b54b1eee72d2727f81e
Warren Van Winckel <warren.van.winckel@gmail.com>
2018-10-25 00:01:56 -0700

    Fix some more spacing

--- actions/wait_for_docker_image.sh
+++ actions/wait_for_docker_image.sh
@@ -15,8 +15,8 @@ if [[ -z "${ORG}" || -z "${IMAGE}" || -z "${VERSION}" ]]; then
 fi
 
 function docker_tag_exists() {
-	URI="https://hub.docker.com/v2/repositories/$1/tags"
-	curl --silent --fail -lSL ${URI} | grep \"${VERSION}\" >/dev/null
+    URI="https://hub.docker.com/v2/repositories/$1/tags"
+    curl --silent --fail -lSL ${URI} | grep \"${VERSION}\" >/dev/null
 }
 
 i=0
