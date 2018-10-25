0512b461b2b219230ba5091c6224eaea4d86da7e
Warren Van Winckel <warren.van.winckel@gmail.com>
2018-10-24 23:44:31 -0700

    Add tries/delay logic

--- actions/wait_for_docker_image.sh
+++ actions/wait_for_docker_image.sh
@@ -5,6 +5,8 @@ set -e
 ORG=$1
 IMAGE=$2
 VERSION=$3
+TRIES=$4
+DELAY=$5
 
 if [[ -z "${ORG}" || -z "${IMAGE}" || -z "${VERSION}" ]]; then
 	echo "ERROR: Missing required parameter"
@@ -17,8 +19,19 @@ function docker_tag_exists() {
     curl --silent --fail -lSL ${URI} | grep \"${VERSION}\" >/dev/null
 }
 
+i=0
 while ! docker_tag_exists "${ORG}/${IMAGE}" $VERSION;
 do
-	echo "Polling every 60s until the ${ORG}/${IMAGE}:${VERSION} image exists."
-	sleep 60
+	if (( ${TRIES} == 0 )); then
+		sleep ${DELAY}
+		continue
+	fi
+	if (( ++i < ${TRIES} )); then
+		sleep ${DELAY}
+    else
+		echo "ERROR: Failed to find image ${ORG}/${IMAGE}:${VERSION} within requested time."
+		exit 1
+	fi
 done
+
+echo "SUCCESS!"
