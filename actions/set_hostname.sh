a99fa2a29060d5b8146bc1e0bdd74e9b134d2c09
Tomaz Muraus <tomaz@tomaz.me>
2019-05-16 11:50:24 +0200

    Add a comment and also fix it for fedora.

--- actions/set_hostname.sh
+++ actions/set_hostname.sh
@@ -23,12 +23,13 @@ ORIGINAL_HOSTNAME=$(hostname)
 if [[ ${DISTRO_LCASE} = "ubuntu" ]]; then
     sed -i -e "s/\(preserve_hostname: \)false/\1true/" /etc/cloud/cloud.cfg && echo "${HOSTNAME}" > /etc/hostname && hostname ${HOSTNAME}
 elif [[ ${DISTRO_LCASE} = "redhat" || ${DISTRO_LCASE} = "centos" ]]; then
-    # Note: We also want to make sure /etc/hostname file matches 
+    # Note: We also want to make sure /etc/hostname file matches
     sed -i -e "s/\(HOSTNAME=\).*/\1${HOSTNAME}/" /etc/sysconfig/network && echo "${HOSTNAME}" > /etc/hostname && hostname ${HOSTNAME}
     hostnamectl set-hostname --static ${HOSTNAME}
+    # Make sure the hostname is preserved between the reboots
     echo "preserve_hostname: true" > /etc/cloud/cloud.cfg.d/99_hostname.cfg
 elif [[ ${DISTRO_LCASE} = "fedora" ]]; then
-    echo -e "HOSTNAME=${HOSTNAME}" >> /etc/sysconfig/network && hostname ${HOSTNAME}
+    echo -e "HOSTNAME=${HOSTNAME}" >> /etc/sysconfig/network && echo "${HOSTNAME}" > /etc/hostname && hostname ${HOSTNAME}
 fi
 
 # For just in case (for scenarios where hostname is not correctly set and preserved during reboots,
