0a16a9306231d5d293b8110d0c4007b958708fa0
armab <dev@arma.biz>
2019-08-15 18:44:57 +0300

    Rework create_vm_windows to use private key from st2kv
    Avoid relying on local files in distributed/HA environment (K8s), use st2kv storage instead

--- actions/aws_decrypt_password_data.py
+++ actions/aws_decrypt_password_data.py
@@ -9,11 +9,11 @@ from st2common.runners.base_action import Action
 
 class AwsDecryptPassworData(Action):
 
-    def run(self, keyfile, password_data):
+    def run(self, private_key, password_data):
         # copied from:
         # https://github.com/aws/aws-cli/blob/master/awscli/customizations/ec2/decryptpassword.py#L96-L122
 
-        self.logger.debug("Decrypting password data using: %s", keyfile)
+        self.logger.debug("Decrypting password data using private_key")
         value = password_data
 
         if not value:
@@ -27,13 +27,10 @@ class AwsDecryptPassworData(Action):
         value = base64.b64decode(value)
 
         try:
-            with open(keyfile) as pk_file:
-                pk_contents = pk_file.read()
-                private_key = rsa.PrivateKey.load_pkcs1(six.b(pk_contents))
-                value = rsa.decrypt(value, private_key)
-                return value.decode('utf-8')
+            rsa_private_key = rsa.PrivateKey.load_pkcs1(six.b(private_key))
+            value = rsa.decrypt(value, rsa_private_key)
+            return value.decode('utf-8')
         except Exception:
-            msg = ('Unable to decrypt password data using '
-                   'provided private key file: {}').format(keyfile)
+            msg = ('Unable to decrypt password data using provided private key')
             self.logger.debug(msg, exc_info=True)
             raise ValueError(msg)
